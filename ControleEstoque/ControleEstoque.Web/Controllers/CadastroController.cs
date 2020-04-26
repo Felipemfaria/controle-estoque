18cb3dc1d5bf86afbca968b9ad66849983f7b87b
Felipe <felipemfaria@outlook.com>
2020-04-26 00:33:20 -0300

    Created POST method to find an specific GrupoProduto

--- ControleEstoque/ControleEstoque.Web/Controllers/CadastroController.cs
+++ ControleEstoque/ControleEstoque.Web/Controllers/CadastroController.cs
@@ -1,17 +1,36 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ControleEstoque.Web.Models;
 
 namespace ControleEstoque.Web.Controllers
 {
     public class CadastroController : Controller
     {
+        private static List<GrupoProdutoModel> _ListaGrupoProduto = new List<GrupoProdutoModel>()
+        {
+            new GrupoProdutoModel(){ id=1, Nome="Canetas", Ativo=true },
+            new GrupoProdutoModel(){ id=2, Nome="Livros", Ativo=true },
+            new GrupoProdutoModel(){ id=3, Nome="Mouses", Ativo=false }       
+        };
+
         [Authorize]
         public ActionResult GrupoProduto()
         {
-            return View();
+            return View(_ListaGrupoProduto);
+        }
+
+        [HttpPost]
+        public ActionResult RecuperarGrupoProduto(int? id)
+        {
+            if (id != null)
+            {
+                return Json(_ListaGrupoProduto.Find(x => x.id == id));
+            }
+
+            return Json("erro: 'ID inválido'");
         }
 
         [Authorize]
