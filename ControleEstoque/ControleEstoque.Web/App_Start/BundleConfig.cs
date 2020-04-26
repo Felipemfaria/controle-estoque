1acc61526d4d2cf45a7a4988130f77bb18fcaeab
Felipe <felipemfaria@outlook.com>
2020-04-26 00:31:10 -0300

    Added and configured bootboxJS library

--- ControleEstoque/ControleEstoque.Web/App_Start/BundleConfig.cs
+++ ControleEstoque/ControleEstoque.Web/App_Start/BundleConfig.cs
@@ -20,11 +20,13 @@ namespace ControleEstoque.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.all.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/GrupoProduto.css"));
         }
     }
 }
