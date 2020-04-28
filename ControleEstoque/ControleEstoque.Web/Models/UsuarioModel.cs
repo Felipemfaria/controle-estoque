a1392168ea7ad17cee106190363d56b1b4fd1678
Felipe <felipemfaria@outlook.com>
2020-04-27 21:25:39 -0300

    Added a try catch block for exception treatment

--- ControleEstoque/ControleEstoque.Web/Models/UsuarioModel.cs
+++ ControleEstoque/ControleEstoque.Web/Models/UsuarioModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
+using System.Web.Mvc;
+using System.Web.Security;
 
 namespace ControleEstoque.Web.Models
 {
@@ -13,17 +15,27 @@ namespace ControleEstoque.Web.Models
             var usuario = false;
             using (var conexao = new SqlConnection())
             {
-                conexao.ConnectionString = @"Data Source = DESKTOP-P80P593\SQLEXPRESS; Initial Catalog = controle_estoque; User Id=admin; Password=123";
-                conexao.Open();
-                using (var comando = new SqlCommand())
+                try
                 {
-                    comando.Connection = conexao;
-                    comando.CommandText = String.Format(
-                        "SELECT COUNT(*) FROM USUARIO WHERE LOGIN='{0}' AND SENHA='{1}'", login, senha);
+                    conexao.ConnectionString = @"Data Source = DESKTOP-P80P593\SQLEXPRESS; Initial Catalog = controle_estoque; User Id=admin; Password=123";
+                    conexao.Open();
+                    using (var comando = new SqlCommand())
+                    {
+                        comando.Connection = conexao;
+                        comando.CommandText = String.Format(
+                            "SELECT COUNT(*) FROM USUARIO WHERE LOGIN='{0}' AND SENHA='{1}'", login, senha);
 
-                    usuario = ((int)comando.ExecuteScalar() > 0);
-
+                        usuario = ((int)comando.ExecuteScalar() > 0);
+                    }
                 }
+                catch (SqlException sqlEx)
+                {
+                    throw sqlEx;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }            
             }
 
             return usuario;
